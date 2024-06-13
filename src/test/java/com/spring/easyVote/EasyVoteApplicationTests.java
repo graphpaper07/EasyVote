e390a4772ce1d894f220b11dd84c57e7c5d36e69
EUNJEONG <graphpaper07@gmail.com>
2024-06-14 00:05:05 +0900

    init project

---
+++ src/test/java/com/spring/easyVote/EasyVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.easyVote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
